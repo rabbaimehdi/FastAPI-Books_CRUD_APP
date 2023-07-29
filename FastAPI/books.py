ecce6c846e404b5de578501d5433d4886664dfb6
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2023-07-29 16:29:19 +0000

    add get endpoints

--- FastAPI/books.py
+++ FastAPI/books.py
@@ -14,3 +14,14 @@ app = FastAPI()
 async def read_all_books():
     return BOOKS
 
+# this app endpoint use the same path as the one with parameters 
+# hence it should always preceed it to avoid validation errors
+@app.get("/books/mybook")
+async def read_favorite_book():
+    return {"book_title" : "My favorite book"}
+
+@app.get("/books/{book_id}")
+async def read_book(book_id:int):
+    return {"book_title" : book_id}
+
+
